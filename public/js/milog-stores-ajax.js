5e6c8ef2994fa0e3d913653407798d1d79fb2ee9
Joanez Andrades <suportedeathofday@gmail.com>
2021-08-30 16:12:15 -0300

    Comitando o arquivo milog-stores-ajax.js

---
+++ public/js/milog-stores-ajax.js
@@ -0,0 +1,57 @@
+
+function milogTicketRequest(el){
+
+    let type    = el.getAttribute('data-action'),
+        orderId = el.getAttribute('data-order-id'),
+        storeId = el.getAttribute('data-store-id');
+
+    var body = {};
+    body.action     = 'milog_store_service_request';
+    body.nonce      = storeAjax.nonce;
+    body.type       = type;
+    body.orderId    = orderId;
+    body.storeId    = storeId;
+
+    jQuery.ajax({
+        url: storeAjax.url,
+        type: 'POST',
+        data: body,
+        beforeSend: function(xhr){
+            console.log('carregando...');
+        },
+        error: function( err ){
+            console.log('[Milog Store Request Error]');
+            console.log(err);
+        }
+    })
+    .done( function( response ){
+        switch (type) {
+            case 'purchase-ticket':
+                if( response == 'success' ){
+                    alert('Compra realizada com sucesso!');
+                    el.setAttribute("disabled", "disabled");
+                } else {
+                    alert('Houve um problema ao comprar a etiqueta. Tente novamente!');
+                }
+                
+                break;
+        
+            case 'print-ticket':
+                console.log(response);
+                window.open( response, '_blank').focus();
+                break;
+
+            case 'tracking-ticket':
+                alert('Recurso desabilitado');
+                break;
+
+            case 'cancel-ticket':
+                alert('Recurso desabilitado')
+                break;
+
+            default:
+                break;
+        }
+        
+    });
+}
